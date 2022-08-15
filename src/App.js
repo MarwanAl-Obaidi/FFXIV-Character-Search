8b2eea9266f75ad71731866e86770127c25f7ed3
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-08-15 11:57:43 +0300

    Repository Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [playerData, setPlayerData] = useState({});
+
+  function searchForPlayer(event) {
+    //console.log("Hello");
+    var APICallString = "https://xivapi.com/character/search?name=" + searchText
+    axios.get(APICallString).then(function(response) {
+      setPlayerData(response.data);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  console.log(playerData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='conatainer'>
+        <h5>FFXIV Player Search</h5>
+        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForPlayer(e)}>Search for player</button>
+      </div>
+      {JSON.stringify(playerData) != '{}' ?
+        <>
+          <p>{<img src={playerData.Results[0].Avatar}/>}</p>
+        </>
+        :
+        <><p>No player data</p></>
+      }
     </div>
   );
 }
